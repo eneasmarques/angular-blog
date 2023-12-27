c718cd0631d719fcb7b87314e1cbccd523816767
Enéas Marques <eneasmarques@outlook.com.br>
2023-12-27 15:34:11 -0300

    feat: add big-card component to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuTitleComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    MenuTitleComponent,
+    HomeComponent,
+    BigCardComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
