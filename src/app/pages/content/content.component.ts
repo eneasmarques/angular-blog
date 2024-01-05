3a289d5ad0d64fcff066ad34bf07c8bc5c84007c
Enéas Marques <eneasmarques@outlook.com.br>
2024-01-05 17:49:01 -0300

    feat: add photo cover, title, and description inputs to Content component

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent {
-
+  @Input() photoCover: string =
+    'https://images.unsplash.com/photo-1701962541409-e2c1256a8574?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+  @Input() contentTitle: string =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+  @Input()
+  contentDescription: string = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+  ullam libero consequatur a, itaque cupiditate voluptatibus iusto molestiae
+  dolorum ipsa blanditiis recusandae. Quibusdam eos est debitis. Distinctio
+  nobis consequuntur praesentium. Lorem ipsum dolor sit amet consectetur
+  adipisicing elit. Exercitationem ullam libero consequatur a, itaque
+  cupiditate voluptatibus iusto molestiae dolorum ipsa blanditiis
+  recusandae. Quibusdam eos est debitis. Distinctio nobis consequuntur
+  praesentium. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+  Exercitationem ullam libero consequatur a, itaque cupiditate voluptatibus
+  iusto molestiae dolorum ipsa blanditiis recusandae. Quibusdam eos est
+  debitis. Distinctio nobis consequuntur praesentium. Lorem ipsum dolor sit
+  amet consectetur adipisicing elit. Exercitationem ullam libero consequatur
+  a, itaque cupiditate voluptatibus iusto molestiae dolorum ipsa blanditiis
+  recusandae. Quibusdam eos est debitis. Distinctio nobis consequuntur
+  praesentium. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+  Exercitationem ullam libero consequatur a, itaque cupiditate voluptatibus
+  iusto molestiae dolorum ipsa blanditiis recusandae. Quibusdam eos est
+  debitis. Distinctio nobis consequuntur praesentium.`;
 }
