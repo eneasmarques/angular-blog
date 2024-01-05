5e803ff299883e907441781c3058e8eac0bbcc49
Enéas Marques <eneasmarques@outlook.com.br>
2024-01-05 20:25:46 -0300

    feat: add dynamic content to content page using dataFake

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,7 +1,7 @@
 export const dataFake = [
   {
     id: 1,
-    photo:
+    photoCover:
       'https://images.unsplash.com/photo-1701962541409-e2c1256a8574?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
@@ -15,7 +15,7 @@ export const dataFake = [
   },
   {
     id: 2,
-    photo:
+    photoCover:
       'https://plus.unsplash.com/premium_photo-1671359582875-a5bcdaf16b14?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
@@ -29,7 +29,7 @@ export const dataFake = [
   },
   {
     id: 3,
-    photo:
+    photoCover:
       'https://media.istockphoto.com/id/1573329496/pt/foto/multi-layers-color-texture-3d-papercut-layers-in-gradient-vector-banner-carving-art-cover.jpg?s=612x612&w=0&k=20&c=808tWIlbQdsTfbZBsSFsMTirtUhO0LaZQGDHGloUbQ4=',
     title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
@@ -43,7 +43,7 @@ export const dataFake = [
   },
   {
     id: 4,
-    photo:
+    photoCover:
       'https://media.istockphoto.com/id/1474394548/pt/foto/dark-blue-abstract-modern-background-geometric-shape-diagonal-lines-gradient.jpg?s=612x612&w=0&k=20&c=AjXV1EwVUYPgy5O9HEv8X2ZASpSj_p6lWuNE0j203sE=',
     title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,32 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
 })
-export class ContentComponent {
-  @Input() photoCover: string =
-    'https://images.unsplash.com/photo-1701962541409-e2c1256a8574?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
-  @Input() contentTitle: string =
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+export class ContentComponent implements OnInit {
+  @Input() photoCover: string = '';
+  @Input() contentTitle: string = '';
   @Input()
-  contentDescription: string = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-  ullam libero consequatur a, itaque cupiditate voluptatibus iusto molestiae
-  dolorum ipsa blanditiis recusandae. Quibusdam eos est debitis. Distinctio
-  nobis consequuntur praesentium. Lorem ipsum dolor sit amet consectetur
-  adipisicing elit. Exercitationem ullam libero consequatur a, itaque
-  cupiditate voluptatibus iusto molestiae dolorum ipsa blanditiis
-  recusandae. Quibusdam eos est debitis. Distinctio nobis consequuntur
-  praesentium. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-  Exercitationem ullam libero consequatur a, itaque cupiditate voluptatibus
-  iusto molestiae dolorum ipsa blanditiis recusandae. Quibusdam eos est
-  debitis. Distinctio nobis consequuntur praesentium. Lorem ipsum dolor sit
-  amet consectetur adipisicing elit. Exercitationem ullam libero consequatur
-  a, itaque cupiditate voluptatibus iusto molestiae dolorum ipsa blanditiis
-  recusandae. Quibusdam eos est debitis. Distinctio nobis consequuntur
-  praesentium. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-  Exercitationem ullam libero consequatur a, itaque cupiditate voluptatibus
-  iusto molestiae dolorum ipsa blanditiis recusandae. Quibusdam eos est
-  debitis. Distinctio nobis consequuntur praesentium.`;
+  contentDescription: string = '';
+  private id: string | null = '0';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => (this.id = value.get('id')));
+
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null): void {
+    const result = dataFake.filter(
+      (article) => article.id.toString() === id
+    )[0];
+
+    if (!!result) {
+      this.photoCover = result.photoCover;
+      this.contentTitle = result.title;
+      this.contentDescription = result.description;
+    }
+  }
 }
