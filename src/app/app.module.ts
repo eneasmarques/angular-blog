896d5cbc406304d979818da09f7ca0032a0b44b1
Enéas Marques <eneasmarques@outlook.com.br>
2024-01-05 17:41:31 -0300

    feat: add `/content` route and `Content` component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { ContentComponent } from './pages/content/content.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
     BigCardComponent,
     SmallCardComponent,
     MenuBarComponent,
+    ContentComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
