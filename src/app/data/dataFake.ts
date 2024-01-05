80e56157810bd008b31bc846ecd1fc3cc6671a64
Enéas Marques <eneasmarques@outlook.com.br>
2024-01-05 18:01:14 -0300

    feat: add dataFake file to provide content data and update /content route to support dynamic content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,58 @@
+export const dataFake = [
+  {
+    id: 1,
+    photo:
+      'https://images.unsplash.com/photo-1701962541409-e2c1256a8574?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+    ullam libero consequatur a, itaque cupiditate voluptatibus iusto molestiae
+    dolorum ipsa blanditiis recusandae. Quibusdam eos est debitis. Distinctio
+    nobis consequuntur praesentium.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+    ullam libero consequatur a, itaque cupiditate voluptatibus iusto molestiae
+    dolorum ipsa blanditiis recusandae. Quibusdam eos est debitis. Distinctio
+    nobis consequuntur praesentium.`,
+  },
+  {
+    id: 2,
+    photo:
+      'https://plus.unsplash.com/premium_photo-1671359582875-a5bcdaf16b14?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+    ullam libero consequatur a, itaque cupiditate voluptatibus iusto molestiae
+    dolorum ipsa blanditiis recusandae. Quibusdam eos est debitis. Distinctio
+    nobis consequuntur praesentium.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+    ullam libero consequatur a, itaque cupiditate voluptatibus iusto molestiae
+    dolorum ipsa blanditiis recusandae. Quibusdam eos est debitis. Distinctio
+    nobis consequuntur praesentium.`,
+  },
+  {
+    id: 3,
+    photo:
+      'https://media.istockphoto.com/id/1573329496/pt/foto/multi-layers-color-texture-3d-papercut-layers-in-gradient-vector-banner-carving-art-cover.jpg?s=612x612&w=0&k=20&c=808tWIlbQdsTfbZBsSFsMTirtUhO0LaZQGDHGloUbQ4=',
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+    ullam libero consequatur a, itaque cupiditate voluptatibus iusto molestiae
+    dolorum ipsa blanditiis recusandae. Quibusdam eos est debitis. Distinctio
+    nobis consequuntur praesentium.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+    ullam libero consequatur a, itaque cupiditate voluptatibus iusto molestiae
+    dolorum ipsa blanditiis recusandae. Quibusdam eos est debitis. Distinctio
+    nobis consequuntur praesentium.`,
+  },
+  {
+    id: 4,
+    photo:
+      'https://media.istockphoto.com/id/1474394548/pt/foto/dark-blue-abstract-modern-background-geometric-shape-diagonal-lines-gradient.jpg?s=612x612&w=0&k=20&c=AjXV1EwVUYPgy5O9HEv8X2ZASpSj_p6lWuNE0j203sE=',
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+    ullam libero consequatur a, itaque cupiditate voluptatibus iusto molestiae
+    dolorum ipsa blanditiis recusandae. Quibusdam eos est debitis. Distinctio
+    nobis consequuntur praesentium.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+    ullam libero consequatur a, itaque cupiditate voluptatibus iusto molestiae
+    dolorum ipsa blanditiis recusandae. Quibusdam eos est debitis. Distinctio
+    nobis consequuntur praesentium.`,
+  },
+];
